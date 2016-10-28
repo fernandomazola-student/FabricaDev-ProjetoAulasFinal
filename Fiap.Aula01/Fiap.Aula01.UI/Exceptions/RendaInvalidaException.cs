31bb7403e29eebf6a543a1edb529ff9311bee018
Thiago Toshiyuki I. Yamamoto <PF0392@fiap.com.br>
2016-10-28 19:24:15 -0200

    Initial Commit

---
+++ Fiap.Aula01/Fiap.Aula01.UI/Exceptions/RendaInvalidaException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Aula01.UI.Exceptions
+{
+
+    [Serializable]
+    public class RendaInvalidaException : Exception
+    {
+        public RendaInvalidaException() { }
+        public RendaInvalidaException(string message) : base(message) { }
+        public RendaInvalidaException(string message, Exception inner) : base(message, inner) { }
+        protected RendaInvalidaException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context)
+        { }
+    }
+}
