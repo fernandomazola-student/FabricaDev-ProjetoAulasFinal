9b205aeb9019ae863598f86cd05b6fce96a25c60
Thiago Toshiyuki I. Yamamoto <PF0392@fiap.com.br>
2016-11-01 18:21:05 -0200

    EF - Cadastro e Listagem

---
+++ Fiap.Exemplo02.MVC.Web/Fiap.Exemplo02.MVC.Web/Controllers/AlunoController.cs
@@ -0,0 +1,36 @@
+﻿using Fiap.Exemplo02.MVC.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exemplo02.MVC.Web.Controllers
+{
+    public class AlunoController : Controller
+    {
+        private PortalContext _context = new PortalContext();
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Aluno aluno)
+        {
+            _context.Aluno.Add(aluno);
+            _context.SaveChanges();
+            TempData["msg"] = "Aluno cadastrado!";
+            return RedirectToAction("Cadastrar");
+        }
+
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            var lista = _context.Aluno.ToList();
+            return View(lista);
+        }
+    }
+}
